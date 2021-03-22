44bce2f7f62a24772f4513a1d521592ebe547c3d
EgorVoron <ekvoron01@gmail.com>
2021-03-22 22:11:28 +0300

    :sparkles: Introducing trees generator 2.0

--- trees_generator.py
+++ trees_generator.py
@@ -1,54 +1,87 @@
 import numpy as np
-from constants import *
-from PIL import Image
-from random_generator import random_with_blocked_values
-from random import randint
+from matplotlib import pyplot as plt
 
-MAP_H = 256
-MAP_W = 256
-possib_map = np.zeros((MAP_H, MAP_W), dtype=np.int)
-trees_map = np.zeros_like(possib_map)
 
-blocked = []
+def perlin(x, y, seed=0):
+    # permutation table
+    np.random.seed(seed)
+    p = np.arange(256, dtype=int)
+    np.random.shuffle(p)
+    p = np.stack([p, p]).flatten()
+    # coordinates of the top-left
+    xi = x.astype(int)
+    yi = y.astype(int)
+    # internal coordinates
+    xf = x - xi
+    yf = y - yi
+    # fade factors
+    u = fade(xf)
+    v = fade(yf)
+    # noise components
+    n00 = gradient(p[p[xi] + yi], xf, yf)
+    n01 = gradient(p[p[xi] + yi + 1], xf, yf - 1)
+    n11 = gradient(p[p[xi + 1] + yi + 1], xf - 1, yf - 1)
+    n10 = gradient(p[p[xi + 1] + yi], xf - 1, yf)
+    # combine noises
+    x1 = lerp(n00, n10, u)
+    x2 = lerp(n01, n11, u)  # FIX1: I was using n10 instead of n01
+    return lerp(x1, x2, v)  # FIX2: I also had to reverse x1 and x2 here
 
-while len(blocked) < (MAP_H - 1) * (MAP_W - 1) - 5:
-    # print(f'blocked_x: {blocked_x}')
-    # print(f'blocked_y: {blocked_y}')
-    # i = random_with_blocked_values(1, MAP_H - 2)
-    # # if not i:
-    # #     continue
-    # j = random_with_blocked_values(1, MAP_W - 2)
-    i = randint(2, MAP_H - 3)
-    j = randint(2, MAP_W - 3)
-    # while (i, j) in blocked:
-    #     print(i, j)
-    #     i = randint(1, MAP_H - 2)
-    #     j = randint(1, MAP_W - 2)
-    blocked.append((i, j))
 
-    # if not j:
-    #     continue
-    neighbors = [possib_map[a][b] for a in range(i - 2, i + 3) for b in range(j - 2, j + 3) if (a, b) != (i, j)]
-    num_ok_neighbors = 0
-    for n in neighbors:
-        if n > 0:
-            num_ok_neighbors += 1
+def lerp(a, b, x):
+    "linear interpolation"
+    return a + x * (b - a)
 
-    if num_ok_neighbors == 0:
-        possib_map[i][j] = np.random.randint(0, 10)
-    elif 1 <= num_ok_neighbors <= 3:
-        possib_map[i][j] = np.random.randint(500, 800)
-    elif 3 <= num_ok_neighbors <= 5:
-        possib_map[i][j] = np.random.randint(400, 600)
-    elif 5 <= num_ok_neighbors <= 8:
-        possib_map[i][j] = np.random.randint(100, 200)
-    elif 8 <= num_ok_neighbors <= 12:
-        possib_map[i][j] = np.random.randint(50, 100)
-    else:
-        possib_map[i][j] = np.random.randint(0, 5)
 
-for y in range(1, MAP_H - 1):
-    for x in range(1, MAP_W - 1):
-        trees_map[y][x] = int(randint(0, 1000) < possib_map[y][x])
+def fade(t):
+    "6t^5 - 15t^4 + 10t^3"
+    return 6 * t ** 5 - 15 * t ** 4 + 10 * t ** 3
 
-Image.fromarray(trees_map * 50).show()
+
+def gradient(h, x, y):
+    "grad converts h to the right gradient vector and return the dot product with (x,y)"
+    vectors = np.array([[0, 1], [0, -1], [1, 0], [-1, 0]])
+    g = vectors[h % 4]
+    return g[:, :, 0] * x + g[:, :, 1] * y
+
+
+# lin = np.linspace(0,5,100,endpoint=False)
+# x,y = np.meshgrid(lin,lin) # FIX3: I thought I had to invert x and y here but it was a mistake
+#
+# plt.imshow(perlin(x,y,seed=2),origin='upper'
+
+
+for i in range(1):
+    lin = np.linspace(0, 5, 1000, endpoint=False)
+    x, y = np.meshgrid(lin, lin)  # FIX3: I thought I had to invert x and y here but it was a mistake
+    # np.random.randint(1, 255)
+    noise = perlin(x, y, seed=np.random.randint(1, 256))
+    # trees_map = np.where(noise < -0.3, 50, 0)
+    # plt.imshow(trees_map, cmap='gray')
+    # plt.show()
+    trees_map = np.zeros((len(noise), len(noise[0])))
+
+    normalized_noise = - (noise / np.linalg.norm(noise) * 100) * 5
+    normalized_noise = np.where(normalized_noise > 1, 1, normalized_noise)
+    normalized_noise = np.where(normalized_noise < -1, -1, normalized_noise)
+
+    # nonlinear_noise = 2*np.tan(normalized_noise) / np.pi
+    # nonlinear_noise = (-normalized_noise + 1) / 12
+    # nonlinear_noise = (np.tanh(-2 * normalized_noise - 0.8) + 1) / 12
+    # nonlinear_noise = (np.tanh(-2 * normalized_noise - 0.8) + 1) / 22
+    nonlinear_noise = (np.tanh(-2 * np.power(normalized_noise, 3) - 1) + 1) / 24
+    # nonlinear_noise = 2 / (30000 * (normalized_noise + 1.084)**2)
+    # nonlinear_noise = 1 / (5000 * (normalized_noise+1.01))
+    #
+
+    # nonlinear_noise = (-normalized_noise + 1) / 2
+    plt.imshow(normalized_noise, origin='upper', cmap='gray')
+    plt.show()
+    for y in range(len(noise)):
+        for x in range(len(noise[y])):
+            trees_map[y][x] = (np.random.randint(0, 100) / 100 < nonlinear_noise[y][x]) * 50
+    # print(np.min(normalized_noise))
+    # print(np.max(normalized_noise))
+    plt.imshow(trees_map, origin='upper', cmap='gray')
+    plt.show()
+    # #
