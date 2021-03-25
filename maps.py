75ae15734a100a460e805b2ec983de857ccb85db
EgorVoron <ekvoron01@gmail.com>
2021-03-25 07:54:07 +0300

    :construction: Working on generators

--- maps.py
+++ maps.py
@@ -3,9 +3,13 @@ from constants import *
 import numpy as np  # заблокируй себе import воздуха
 from PIL import Image
 from math import sin, cos, sqrt
-from random import randint
+from copy import copy
+from random import randint, shuffle, choice
 from random_generator import random_point_with_blocked_square
 from matplotlib import pyplot as plt
+from core.generators.ore_generator import gen_ore_matrix
+from core.virtual_objects.raw_materials.raw_materials import IronBatch, CopperBatch, StoneBatch, CoalBatch, TreeBatch
+
 
 def abs_coordinates(x, y):
     return x + MAP_W // 2, y + MAP_H // 2
@@ -15,14 +19,17 @@ def dist(point1: tuple, point2: tuple) -> float:
     return sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
 
-def point_on_ore(x, y, ore):
-    return ore[0][0] < x < ore[1][0] and ore[0][1] < y < ore[1][1]
+class MapCell:
+    def __init__(self, category):
+        self.category = category
+        self.usable_object = None
+        self.raw_material_batch = None
 
 
 class Map(ABC):
     height = MAP_H
     width = MAP_W
-    map_matrix = [[0] * MAP_W] * MAP_H
+    map_matrix = [[None] * MAP_W] * MAP_H
     num_ores = int
     inner_square_parts: list
     ore_size: int
@@ -34,18 +41,17 @@ class Map(ABC):
         inner_square_size_half = inner_square_size // 2
         inner_square_bounds = (
             (-inner_square_size_half, -inner_square_size_half), (inner_square_size_half, inner_square_size_half))
+        print(inner_square_bounds)
         inner_square_mtx_bounds = (abs_coordinates(*inner_square_bounds[0]), abs_coordinates(*inner_square_bounds[1]))
-
+        print('Mm', inner_square_mtx_bounds)
         basic_square_size_half = 128  # CONST
         basic_square_bounds = (
             (-basic_square_size_half, -basic_square_size_half), (basic_square_size_half, basic_square_size_half))
         basic_square_mtx_bounds = (abs_coordinates(*basic_square_bounds[0]), abs_coordinates(*basic_square_bounds[1]))
+        print('b', basic_square_mtx_bounds)
 
         def gen_circle_rad(diagonal) -> int:
             return int((randint(*self.radius_coef) / 10) * diagonal)
-
-        def dist(point1: tuple, point2: tuple) -> float:
-            return sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
         def point_on_ore(x, y, ore):
             return ore[0][0] < x < ore[1][0] and ore[0][1] < y < ore[1][1]
@@ -94,7 +100,7 @@ class Map(ABC):
                 x, y = random_point_with_blocked_square(outer_ore_size, outer_ore_size, MAP_W - outer_ore_size,
                                                         MAP_H - outer_ore_size, blocked_square)
                 for gen_ore_center in outer_ores_centers:
-                    while dist((x, y), gen_ore_center) <= int(outer_ore_diagonal * 2):
+                    while dist((x, y), gen_ore_center) <= int(outer_ore_diagonal * 4):
                         x, y = random_point_with_blocked_square(outer_ore_size, outer_ore_size, MAP_W - outer_ore_size,
                                                                 MAP_H - outer_ore_size, blocked_square)
                 outer_ores_centers.append((x, y))
@@ -115,39 +121,55 @@ class Map(ABC):
 
         inner_ores_bounds, outer_ores_bounds = gen_ore_centers()
 
-        matix = np.array([[0] * MAP_W] * MAP_H)
-        for ore in inner_ores_bounds:
-            ore_matrix = np.array([[0] * self.ore_size] * self.ore_size)
-            ore_matrix = np.random.rand(32, 32)
-            ore_matrix = np.where(ore_matrix < 0.333, 0, np.random.rand(32, 32) * 300)
-            # ore
-            mt10 = (np.random.rand(10, 10) / 5 + 0.5) * 1000
-            mt16 = np.zeros((16, 16))
-            mt16[3:13, 3:13] = mt10
-            mt16 = np.where(mt16 == 0, (np.random.rand(16, 16) / 5 + 0.3) * 1000, mt16)
-            mt28 = np.zeros((28, 28))
-            mt28[6:22, 6:22] = mt16
-            # possib28 = np.random.rand(28, 28) * 0.6
-            mt28 = np.where((mt28 == 0), ((np.random.rand(28, 28) / 2.5 + 0.2) - 0.4) * 1000, mt28)
-            mt28 = np.where((mt28 < 0),0, mt28)
+        matrix = np.array([[0] * MAP_W] * MAP_H)
+        print(outer_ores_bounds)
 
-            mt32 = -np.ones((32, 32))
-            mt32[2:30, 2:30] = mt28
-            # possib32 = np.random.rand(32, 32) * 0.3
-            mt32 = np.where((mt32 == -1), (np.random.rand(32, 32) / 5) * 1000, mt32)
-            plt.imshow(mt32)
-            plt.show()
-        # ores_bounds = inner_ores_bounds + outer_ores_bounds
-        # pic_mtx = np.array([[0] * MAP_W] * MAP_H)
-        # for y in range(len(mt16)):
-        #     for x in range(len(mt16[y])):
-        # #         for i in range(len(ores_bounds)):
-        # #             if point_on_ore(x, y, ores_bounds[i]):
-        # #                 pic_mtx[y][x] = 50 if i < 4 else 70
-        # # if point_on_ore(x, y, basic_square_mtx_bounds):
-        # #     pic_mtx[y][x] += 10
-        #
-        # Image.fromarray(pic_mtx).show()
+        batch_types = [IronBatch, CoalBatch, CopperBatch, StoneBatch]
+        shuffle(batch_types)
+
+        ores_with_types = []
+        for idx, ore_bounds in enumerate(inner_ores_bounds):
+            ores_with_types.append([ore_bounds, batch_types[idx]])
+
+        for ore_bounds in outer_ores_bounds:
+            ores_with_types.append([ore_bounds, choice(batch_types)])
+
+        # TODO:
+        """
+        * gen_ore_matrix()
+        * generate surface_noise
+        * interpret_surface_noise()
+        * generate trees
+        """
+
+        def interpret_surface_noise():
+            pass
+
+        def surface_noise():
+            pass
+
+        trees = None
+
+        for i in range(len(ores_with_types)):
+            ores_with_types[i].append(gen_ore_matrix(self.ore_size))
+
+        def create_batch_for_cell(x, y):
+            for bounds, batch_type, ore_matrix in ores_with_types:
+                if point_on_ore(x, y, bounds):
+                    return batch_type(ore_matrix[x][y])
+
+        for y in range(len(MAP_H)):
+            for x in range(MAP_W):
+                cell = MapCell(interpret_surface_noise(surface_noise[y][x]))
+                # фабричны метод (зачем....)
+                # raw_material_gen, amount = get_ore_of_cell(x, y)
+                # if raw_material_gen:
+                #     cell.raw_material_batch = raw_material_gen.generate_raw_material(amount)
+                if trees[y][x]:
+                    cell.raw_material_batch = TreeBatch(randint(10, 20))  # CONST
+                else:
+                    cell.raw_material_batch = create_batch_for_cell(x, y)
+                self.map_matrix[y][x] = cell
 
 
 class EasyMap(Map):
@@ -197,5 +219,33 @@ class HardMapCreator(MapCreator):
         return HardMap()
 
 
-creator = EasyMapCreator()
-creator.gen_map()
+if __name__ == '__main__':
+    creator = EasyMapCreator()
+    creator.gen_map()
+#
+
+#
+#
+# def noise_to_kind():
+#     pass
+#
+#
+#
+#
+# def get_ore_of_cell(x, y):
+#     pass
+#
+#
+# for y in range(len(MAP_H)):
+#     for x in range(MAP_W):
+#         cell = MapCell(interpret_surface_noise(surface_noise[y][x]))
+#         # фабричны метод (зачем....)
+#         # raw_material_gen, amount = get_ore_of_cell(x, y)
+#         # if raw_material_gen:
+#         #     cell.raw_material_batch = raw_material_gen.generate_raw_material(amount)
+#
+#         cell.raw_material_batch = create_batch_for_cell(x, y)
+#         map[y][x] = cell
+#
+#
+# def create_batch_for_cell(x, y):
