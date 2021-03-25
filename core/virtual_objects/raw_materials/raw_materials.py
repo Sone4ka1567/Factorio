8a86505446da3df15a6c1a267e7ade567dd5ab01
EgorVoron <ekvoron01@gmail.com>
2021-03-25 07:53:28 +0300

    :zap: Add materials after finishing architecture

--- core/virtual_objects/raw_materials/raw_materials.py
+++ core/virtual_objects/raw_materials/raw_materials.py
@@ -1,13 +1,69 @@
 from base_classes import VirtualObject
 
 
-class RawMaterial(VirtualObject):
-    amount: int
-
-
-class Iron(RawMaterial):
+class MaterialBatch(VirtualObject):
     def __init__(self, amount):
         self.amount = amount
 
-    def convert_to_plates(self):
-        pass
+    def __copy__(self):
+        return MaterialBatch(self.amount)
+
+
+class RawMaterialBatch(MaterialBatch):
+    def __init__(self, amount, intermediate_class=None):
+        super().__init__(amount)
+        self.intermediate_class = intermediate_class
+
+    def __copy__(self):
+        return RawMaterialBatch(self.amount, self.intermediate_class)
+
+
+class IronPlatesBatch(MaterialBatch):
+    pass
+
+
+class IronBatch(RawMaterialBatch):
+    def __init__(self, amount):
+        super().__init__(amount)
+        self.intermediate_class = IronPlatesBatch
+
+
+class CopperPlatesBatch(MaterialBatch):
+    pass
+
+
+class CopperBatch(RawMaterialBatch):
+    def __init__(self, amount):
+        super().__init__(amount)
+        self.intermediate_class = CopperPlatesBatch
+
+
+class CoalBatch(RawMaterialBatch):
+    def __init__(self, amount):
+        super().__init__(amount)
+        self.intermediate_class = None  # ??????????????
+
+
+class StoneBricksBatch(MaterialBatch):
+    pass
+
+
+class StoneBatch(RawMaterialBatch):
+    def __init__(self, amount):
+        super().__init__(amount)
+        self.intermediate_class = StoneBricksBatch
+
+
+class TreeBatch(RawMaterialBatch):
+    def __init__(self, amount):
+        super().__init__(amount)
+        self.intermediate_class = None  # ??????????????
+
+# def exchange(input_batch: RawMaterialBatch, output_batch: RawMaterialBatch, amount: int):
+#     if not isinstance(output_batch, type(input_batch)):
+#         return False
+#     else:
+#         possible_amount = min(input_batch.amount, amount)
+#         input_batch.amount -= possible_amount
+#         output_batch.amount += possible_amount
+#         return True
