bb70a99ba36f4c40cc3f45e1eeb379492f6da1e0
EgorVoron <ekvoron01@gmail.com>
2021-04-10 20:46:19 +0300

    :zap: Update Container

--- core/virtual_objects/container.py
+++ core/virtual_objects/container.py
@@ -12,9 +12,13 @@ class Container:
         return False
 
     def add(self, batch):
+        done = False
         for idx, cur_batch in enumerate(self.data):
             if isinstance(cur_batch, type(batch)):
                 self.data[idx].amount += batch.amount
+                done = True
+        if not done:
+            self.data.append(batch)
 
     def remove(self, batch):
         for idx, cur_batch in enumerate(self.data):
@@ -22,13 +26,30 @@ class Container:
                 if cur_batch.amount > batch.amount:
                     self.data[idx].amount -= batch.amount
                 else:
-                    self.data[idx] = None
+                    self.data.pop(idx)
 
     def get_data(self):
         return self.data
 
     def copy(self):
         return Container(deepcopy(self.data))
+
+    def can_produce(self, target_batch):
+        bag_copy = self.copy()
+        res = target_batch.count_requirements(bag_copy)
+        if res:
+            self.data = bag_copy.data
+            print('without res: ', self)
+            return True
+        print('impossible')
+        return False
+
+    def produce_inside(self, target_batch):
+        if self.can_produce(target_batch):
+            self.add(target_batch)
+            print('with res:', self)
+            return True
+        return False
 
     def __str__(self):
         if self.data:
