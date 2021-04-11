be350748fb0803b73d5341b15367588ef94e4c00
EgorVoron <ekvoron01@gmail.com>
2021-04-11 23:42:45 +0300

    :sparkles: Add Proxy pattern to map_objects

---
+++ core/map_objects/abstracts.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from base_classes import MapObject
+
+
+class UsableObjectInterface(ABC):
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class UsableObject(ABC, UsableObjectInterface, MapObject):
+    pass
+
+
+class UsableObjectProxy(UsableObjectInterface):
+    def __init__(self, real_object: UsableObject):
+        self._real_object = real_object
+        self.progress = 0
+
+    def _tick_react(self):
+        self.progress += 1
+
+    def run(self):
+        self._tick_react()
+        self._real_object.run()
