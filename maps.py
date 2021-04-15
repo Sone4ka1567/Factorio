12f7e35dae127102dd53dadcff84573db3f1bb6e
EgorVoron <ekvoron01@gmail.com>
2021-04-15 20:50:20 +0300

    :bug: Fix np.int overflow

--- maps.py
+++ maps.py
@@ -31,7 +31,7 @@ class Map(ABC):
 
     def __init__(self):
         self.map_objects = {}
-        self.map_matrix = np.zeros((self.height, self.width), dtype=np.int)
+        self.map_matrix = np.zeros((self.height, self.width), dtype=np.int64)
 
     def get_map_objects(self):
         return self.map_objects
